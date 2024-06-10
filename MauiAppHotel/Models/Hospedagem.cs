00d88c5be7beeecd1e1403adae81e14c615cac5c
Usuario <Usuario@CPD03-M15>
2024-06-10 15:06:01 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppHotel/Models/Hospedagem.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiAppHotel.Models
+{
+    public class Hospedagem
+    {
+        public Quarto QuartoSelecionado { get; set; }
+
+        public int QntAdultos { get; set; } 
+
+        public int QntCriancas {get; set; } 
+
+        public DateTime DataCheckin { get; set; }
+
+        public DateTime DataCheckout { get; set; }
+
+        public int Estadia
+        {
+            get => DataCheckout.Subtract(DataCheckin).Days;
+        }
+
+        public double ValorTotal
+        {
+            get
+            {
+                double valor_adultos = QntAdultos * QuartoSelecionado.ValorDiariaAdulto;
+                double valor_criancas = QntCriancas * QuartoSelecionado.ValorDiariaCrianca;
+
+                double total = (valor_adultos + valor_criancas) * Estadia;
+
+                return total;
+            }
+        }
+    }
+}
